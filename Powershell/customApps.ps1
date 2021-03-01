a7d620eca24d427184c144b5410d1e373b871200
zcc <listenzcc@mail.bnu.edu.cn>
2021-03-01 16:37:10 +0800

    Add function in powershell package.
    Add param manager for functions in powershell;
    Add easy-to-use file type.

--- Powershell/customApps.ps1
+++ Powershell/customApps.ps1
@@ -4,19 +4,25 @@
 function MyServer {
     # Login to $ServerIP as $ServerUser
     # $args commands will be operated if being provided
-    Write-Output "ssh -l $ServerUser $ServerIP $args"
-    ssh -l $ServerUser $ServerIP $args
+    [cmdletBinding()]
+    param (
+        $User = $serverUser
+        , $Ip = $serverIP
+        , $Cmd
+    )
+    Write-Output "ssh -l $User $Ip $Cmd"
+    ssh -l $User $Ip $Cmd
 }
 
 function Start-Bgd-Job {
     # Start background job
-    if ($args.length -eq 0) {
-        Write-Output 'Recommand gramma:'
-        Write-Output 'Start-Bgd-Job -jupyter'
-    }
+    [cmdletBinding()]
+    param(
+        $Method
+    )
 
     # Start jupyter as background job
-    if ($args -eq "-jupyter") {
+    if ($Method -eq "jupyter") {
         Write-Output 'Starting juptyerlab'
         Start-Job -ScriptBlock { Set-Location $using:pwd; jupyter-lab.exe . }
     }
@@ -25,33 +31,29 @@ function Start-Bgd-Job {
 function Remove-Bgd-Jobs {
     # Remove all jobs by force
     # It means kill the job and remove it from the pool
+    # Show State Before Remove
     Write-Output 'Before:'
     Get-Job
+    # Remove jobs
     Get-Job | Select-Object id | Remove-Job -Force
+    # Show State After Remove
     Write-Output 'After:'
     Get-Job
 }
 
-function Find-Files-By ($ext, $depth, $combine) {
+function Find-Files-By {
     # Find all the files ends with $ext
     # if not provide $depth,
     # we will use 3 for instead.
+    [CmdletBinding()]
+    param(
+        $Ext = ".txt"
+        , $Depth = 1
+    )
 
-    if ($depth.length -eq 0) {
-        $depth = 3
-    }
+    Write-Output "----  Finding $Ext files with Depth of $Depth ----"
 
-    if ($ext.length -eq 0) {
-        Write-Output 'Invalid input, Example input is "Find-Files-By $ext[, $depth]"'
-    }
-    else {
-        if ($combine -eq 0) {
-            Get-ChildItem -depth $depth -Recurse | Where-Object { $_.Extension -eq $ext } | Sort-Object FullName
-        }
-        else {
-            Get-ChildItem -depth $depth -Recurse | Where-Object { $_.Extension -eq $ext } | Sort-Object FullName | Select-Object LastWriteTime, fullname
-        }
-    }
+    Get-ChildItem -Depth $depth -Recurse | Where-Object { $_.Extension -eq $ext } | Sort-Object FullName
 }
 
 function Invoke-CapsLockPlus () {
@@ -60,13 +62,29 @@ function Invoke-CapsLockPlus () {
     Invoke-Command -ScriptBlock { set-location $env:HOME\Documents\capslock-plus; .\CapsLock+.ahk }
 }
 
+function Get-ChildItem-Enhance {
+    # Enhancement of Get-ChildItem method
+    # It will produce "MegaByte" and "RelativeName" properties for Get-ChildItem
+    [CmdletBinding()]
+    param(
+        $Path = $pwd.path
+        , $Depth = 0
+        , [switch]$Recurse
+    )
+
+    $gc = Get-ChildItem -Path $Path -Depth $Depth -Recurse
+
+    $gc | Select-Object Mode, LastWriteTime, FileSize, @{Name = "RelativeName"; Expression = { (resolve-path -relative $_.fullname) } }
+}
+
 function MyFunctions() {
     # List all my custom functions
-    Write-Output "MyServer            `t: Access to my Server Quickly"
-    Write-Output "Start-Bgd-job       `t: Start Background Job"
-    Write-Output "Remove-Bgd-Jobs     `t: Remove All Background Jobs"
-    Write-Output "Find-Files-By       `t: Find Subfiles with their Extensions"
-    write-Output "Invoke-CapsLockPlus `t: Invoke CapsLockPlus"
+    Write-Output "MyServer              `t: Access my Server Quickly"
+    Write-Output "Start-Bgd-job         `t: Start Background Job"
+    Write-Output "Remove-Bgd-Jobs       `t: Remove All Background Jobs"
+    Write-Output "Find-Files-By         `t: Find Subfiles with their Extensions"
+    Write-Output "Invoke-CapsLockPlus   `t: Invoke CapsLockPlus"
+    Write-Output "Get-ChildItem-Enhance `t: List Files in Enhancement"
 }
 
 Write-Output 'Custom Apps have been loaded, type "MyFunctions" to see them.'
