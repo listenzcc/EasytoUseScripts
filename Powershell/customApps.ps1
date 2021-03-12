3ebf5ee7a113431e47be5af9a3be3d927cabf052
zcc <listenzcc@mail.bnu.edu.cn>
2021-03-12 14:51:42 +0800

    Improve powershell script
    Add Get-ChildItem-MyEnhance, Set-Location-MyEnhance, Get-LocationTrace

--- Powershell/customApps.ps1
+++ Powershell/customApps.ps1
@@ -62,29 +62,90 @@ function Invoke-CapsLockPlus () {
     Invoke-Command -ScriptBlock { set-location $env:HOME\Documents\capslock-plus; .\CapsLock+.ahk }
 }
 
-function Get-ChildItem-Enhance {
+function Get-ChildItem-MyEnhance {
     # Enhancement of Get-ChildItem method
     # It will produce "MegaByte" and "RelativeName" properties for Get-ChildItem
     [CmdletBinding()]
     param(
         $Path = $pwd.path
         , $Depth = 0
+        , $Exclude = ""
         , [switch]$Recurse
     )
 
     $gc = Get-ChildItem -Path $Path -Depth $Depth -Recurse
 
-    $gc | Select-Object Mode, LastWriteTime, FileSize, @{Name = "RelativeName"; Expression = { (resolve-path -relative $_.fullname) } }
+    $gc | Select-Object Mode, LastWriteTime, FileSize, @{Name = "RelativeName"; Expression = { (resolve-path -relative $_.fullname).Replace(".\", "") } }
 }
 
+function Set-Location-MyEnhance {
+    # Require,
+    # module: cd-extras
+
+    [CmdletBinding()]
+    param(
+        # Default $Path
+        $Path = '~'
+    )
+
+    # CD to $Path and prompt
+    Set-LocationEx $Path
+    $path = $pwd.path
+    write-output "Changed location to $path"
+
+    # Append the $path,
+    # into the END of the file
+    $path >> $env:HOME/.cd-trace
+}
+
+
+function Get-LocationTrace {
+    # Require,
+    # module: cd-extras
+
+    # Get trace history
+    $lst = Get-Content $env:HOME/.cd-trace
+    # Reverse the trace
+    [array]::Reverse($lst)
+    # Remove the duplicated rows,
+    # the first occurs are kept
+    $lst = ($lst | Select-Object -Unique)
+    # Reverse back the trace
+    [array]::Reverse($lst)
+    $lst = ($lst | Select-Object -Last 9)
+    # See what we have got
+    # write-output $lst $lst.Length
+    For ($i = 0; $i -lt $lst.Length; $i++) {
+        $j = $lst.Length - $i
+        $e = $lst[$i]
+        Write-Output "[$j] $e"
+    }
+
+    # Interaction
+    $select = Read-Host -Prompt 'CD to'
+    $path = $lst[$lst.Length - $select]
+    Write-Output $path
+    Set-Location-MyEnhance $path
+}
+
+$ScriptPath = $script:MyInvocation.MyCommand.Path
+
+Set-Item Alias:cd Set-Location-MyEnhance
+Set-Alias cdt Get-LocationTrace
+Update-TypeData -AppendPath $ScriptPath\\..\\type\\fileTypeEnhance.ps1xml -verbose
+
 function MyFunctions() {
+    # The script is
+    Write-Output "Script is $ScriptPath"
     # List all my custom functions
-    Write-Output "MyServer              `t: Access my Server Quickly"
-    Write-Output "Start-Bgd-job         `t: Start Background Job"
-    Write-Output "Remove-Bgd-Jobs       `t: Remove All Background Jobs"
-    Write-Output "Find-Files-By         `t: Find Subfiles with their Extensions"
-    Write-Output "Invoke-CapsLockPlus   `t: Invoke CapsLockPlus"
-    Write-Output "Get-ChildItem-Enhance `t: List Files in Enhancement"
+    Write-Output "MyServer                `t: Access my Server Quickly"
+    Write-Output "Start-Bgd-job           `t: Start Background Job"
+    Write-Output "Remove-Bgd-Jobs         `t: Remove All Background Jobs"
+    Write-Output "Find-Files-By           `t: Find Subfiles with their Extensions"
+    Write-Output "Invoke-CapsLockPlus     `t: Invoke CapsLockPlus"
+    Write-Output "Get-ChildItem-MyEnhance `t: List Files with their FileSize"
+    Write-Output "Set-Location-MyEnhance  `t: Enhanced cd command, cd and record the pwd after"
+    Write-Output "Get-LocationTrace       `t: Enhanced cdt command, list history path and cd to the selected"
 }
 
 Write-Output 'Custom Apps have been loaded, type "MyFunctions" to see them.'
